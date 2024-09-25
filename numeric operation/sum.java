a819291048692b34a006d775e3c676562527f255
Aryan Goyal <150564298+Aryan-goyal9950@users.noreply.github.com>
2024-09-25 20:11:09 +0530

    Add files via upload

---
+++ numeric operation/sum.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class sum
+{	public static void main(String s[])
+{
+	Scanner sc=new Scanner(System.in);
+	System.out.println("enter two numbers : ");
+	int a1=sc.nextInt();
+	int a2=sc.nextInt();
+	int sum=a1+a2;
+	System.out.print("sum : " + sum);
+	
+}
+}
